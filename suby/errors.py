9a98b9ecfdc7019bac8628fd60c3e54a657dc023
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-13 23:27:02 +0300

    exception init

--- suby/errors.py
+++ suby/errors.py
@@ -4,4 +4,4 @@ from suby.subprocess_result import SubprocessResult
 class RunningCommandError(Exception):
     def __init__(self, message: str, subprocess_result: SubprocessResult) -> None:
         self.result = subprocess_result
-        super().__init__(self, message)
+        super().__init__(message)
