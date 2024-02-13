7a886b603cd319a33466829e520b84fe0f838c5b
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-13 21:24:38 +0300

    +1 reimport

--- suby/__init__.py
+++ suby/__init__.py
@@ -1,6 +1,7 @@
 import sys
 
 from suby.proxy_module import ProxyModule as ProxyModule
+from suby.errors import RunningCommandError as RunningCommandError  # noqa: F401
 
 
 sys.modules[__name__].__class__ = ProxyModule
