439056087956da1dd90e47bc7714e4746c350aaf
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-13 23:10:46 +0300

    tests

--- tests/test_proxy_module.py
+++ tests/test_proxy_module.py
@@ -1,10 +1,13 @@
 import sys
 from time import perf_counter
+from io import StringIO
+from contextlib import redirect_stdout, redirect_stderr
 
 import pytest
 from cantok import TimeoutCancellationError
 
 import suby
+from suby import RunningCommandError
 
 
 def test_normal_way():
@@ -64,3 +67,42 @@ def test_timeout_without_catching_exception():
 
     assert (end_time - start_time) < sleep_time
     assert (end_time - start_time) >= timeout
+
+
+def test_exception_in_subprocess_without_catching():
+    with pytest.raises(RunningCommandError, match=f'Error when executing the command "{sys.executable} -c "raise ValueError"".'):
+        suby(sys.executable, '-c', 'raise ValueError')
+
+    try:
+        suby(sys.executable, '-c', 'raise ValueError')
+    except RunningCommandError as e:
+        assert e.result.stdout == ''
+        assert 'ValueError' in e.result.stderr
+        assert e.result.returncode != 0
+
+
+def test_not_catching_output():
+    stderr_buffer = StringIO()
+    stdout_buffer = StringIO()
+
+    with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
+        result = suby(sys.executable, '-c', 'print("kek1", end=""); import sys; sys.stderr.write("kek2")', catch_output=False)
+
+        stderr = stderr_buffer.getvalue()
+        stdout = stdout_buffer.getvalue()
+
+        assert result.returncode == 0
+        assert stderr == 'kek2'
+        assert stdout == 'kek1'
+
+
+def test_catching_output():
+    stderr_buffer = StringIO()
+    stdout_buffer = StringIO()
+
+    with redirect_stdout(stdout_buffer), redirect_stderr(stderr_buffer):
+        result = suby(sys.executable, '-c', 'print("kek1", end=""); import sys; sys.stderr.write("kek2")', catch_output=True)
+
+        assert result.returncode == 0
+        assert stderr_buffer.getvalue() == ''
+        assert stdout_buffer.getvalue() == ''
