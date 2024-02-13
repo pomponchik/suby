225606b67fc83b94b31cb1bbaec80ca88badf954
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-13 17:23:12 +0300

    +1 test

--- tests/test_proxy_module.py
+++ tests/test_proxy_module.py
@@ -1,6 +1,9 @@
 import sys
 from time import perf_counter
 
+import pytest
+from cantok import TimeoutCancellationError
+
 import suby
 
 
@@ -38,6 +41,26 @@ def test_timeout():
     assert result.returncode != 0
     assert result.stdout == ''
     assert result.stderr == ''
+
+    assert (end_time - start_time) < sleep_time
+    assert (end_time - start_time) >= timeout
+
+
+def test_timeout_without_catching_exception():
+    sleep_time = 100000
+    timeout = 0.001
+
+    with pytest.raises(TimeoutCancellationError):
+        suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', timeout=timeout)
+
+    start_time = perf_counter()
+    try:
+        result = suby(sys.executable, '-c', f'import time; time.sleep({sleep_time})', timeout=timeout)
+    except TimeoutCancellationError as e:
+        assert e.result.stdout == ''
+        assert e.result.stderr == ''
+        assert e.result.returncode != 0
+    end_time = perf_counter()
 
     assert (end_time - start_time) < sleep_time
     assert (end_time - start_time) >= timeout
