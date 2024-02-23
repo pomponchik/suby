0a72cab1644ce5d7ace16a3ed693413f9c737070
Evgeniy Blinov <zheni-b@yandex.ru>
2024-02-23 16:43:28 +0300

    reimport of the TimeoutCancellationError from the cantok

--- suby/__init__.py
+++ suby/__init__.py
@@ -3,5 +3,7 @@ import sys
 from suby.proxy_module import ProxyModule as ProxyModule
 from suby.errors import RunningCommandError as RunningCommandError  # noqa: F401
 
+from cantok import TimeoutCancellationError as TimeoutCancellationError  # noqa: F401
+
 
 sys.modules[__name__].__class__ = ProxyModule
